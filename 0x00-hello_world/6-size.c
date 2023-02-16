2e155a287cac70ae1031b583422a52edffd9ee52
alantiren <tirenalan0@gmail.com>
2023-02-17 00:57:36 +0300

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - A program that prints a line with puts function
+* Return: 0 (Success)
+*/
+
+int main(void)
+{
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("Size of an int: %zu bytes(s)\n", sizeof(int));
+printf("Size of a long init: %zu byte(s)\n", sizeof(long init));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long long init));
+printf("Size of a float: %zu byte(s)\n", sizeof(float));
+return (0);
+}
+
