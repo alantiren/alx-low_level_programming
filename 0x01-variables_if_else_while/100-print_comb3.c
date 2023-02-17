f61e0d57c07ec48dcd4a8483f2fefdae9af071b8
alantiren <tirenalan0@gmail.com>
2023-02-17 17:09:30 +0300

    print comb

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,14 +12,18 @@ int d, p;
 
 for (d = '0'; d < '9'; d++)
 {
+
 for (p = d + 1; p <= '9'; p++)
-if (p !=d)
+{if (p !=d)
+{
 putchar(d);
 putchar(p);
 if (d == '8' && p == '9')
 continue;
 putchar(',');
 putchar(' ');
+}
+}
 }
 putchar('\n');
 return (0);
