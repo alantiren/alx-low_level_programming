cb0476b2116f30c7fca711da44e56f341f6eaf0e
alantiren <tirenalan0@gmail.com>
2023-02-17 16:33:36 +0300

    print

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -19,22 +19,21 @@ int main(void)
 int a;
 int d = 0;
 
-while (d < 10;){
+while (d < 10)
+{
 c = 0;
 }
 while (c < 10){
 if (d != c && d < c)
-}
-{
 putchar('0' + d);
 putchar('0' + c);
 if (c + d != 17)
 {
 putchar(',');
 putchar(' ');
+}
 c++;
 d++;
-}
 }
 return (0);
 }
