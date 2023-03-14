4d3ea730f007ab7f316c5133a9fa8475b9aadaad
alantiren <122862761+alantiren@users.noreply.github.com>
2023-03-14 15:36:27 +0300

    task 2

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,4 +1,4 @@
-#include <stdio.h>	
+#include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
 
@@ -12,7 +12,7 @@ char *_strdup(char *str)
 {
 char *aaa;
 int i, r = 0;
-		
+
 if (str == NULL)
 return (NULL);
 i = 0;
