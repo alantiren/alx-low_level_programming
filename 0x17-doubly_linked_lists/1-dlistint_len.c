b462903456568c057d6114abaf96af8549e003cd
alantiren <tirenalan0@gmail.com>
2023-05-18 22:04:31 +0300

    updates

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -2,14 +2,12 @@
 
 size_t dlistint_len(const dlistint_t *h)
 {
-    const dlistint_t *current = h;
-    size_t count = 0;
-
-    while (current != NULL)
-    {
-        count++;
-        current = current->next;
-    }
-
-    return count;
+const dlistint_t *current = h;
+size_t count = 0;
+while (current != NULL)
+{
+count++;
+current = current->next;
+}
+return (count);
 }
--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,33 +10,33 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *new_node, *current = *h;
-    unsigned int i;
+dlistint_t *new_node, *current = *h;
+unsigned int i;
 
-    if (h == NULL)
-        return NULL;
+if (h == NULL)
+return (NULL);
 
-    if (idx == 0)
-        return add_dnodeint(h, n);
+if (idx == 0)
+return (add_dnodeint(h, n));
 
-    for (i = 0; i < idx - 1 && current != NULL; i++)
-        current = current->next;
+for (i = 0; i < idx - 1 && current != NULL; i++)
+current = current->next;
 
-    if (current == NULL)
-        return NULL;
+if (current == NULL)
+return (NULL);
 
-    if (current->next == NULL)
-        return add_dnodeint_end(h, n);
+if (current->next == NULL)
+return (add_dnodeint_end(h, n));
 
-    new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
-        return NULL;
+new_node = malloc(sizeof(dlistint_t));
+if (new_node == NULL)
+return (NULL);
 
-    new_node->n = n;
-    new_node->prev = current;
-    new_node->next = current->next;
-    current->next->prev = new_node;
-    current->next = new_node;
+new_node->n = n;
+new_node->prev = current;
+new_node->next = current->next;
+current->next->prev = new_node;
+current->next = new_node;
 
-    return new_node;
+return (new_node);
 }
