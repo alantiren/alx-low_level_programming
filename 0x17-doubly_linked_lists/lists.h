a6fb9892f3add1c8ebea064ed223430f74aa8d4e
alantiren <tirenalan0@gmail.com>
2023-05-18 21:23:31 +0300

    updates

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -23,31 +23,15 @@ typedef struct dlistint_s
 
 /* Function Prototypes */
 
-/* Function to print the elements of a doubly linked list */
+size_t print_dlistint(const dlistint_t *h)
 void print_dlistint(const dlistint_t *head);
-
-/* Function to add a new node at the beginning of a doubly linked list */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
-
-/* Function to add a new node at the end of a doubly linked list */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
-
-/* Function to free a doubly linked list */
 void free_dlistint(dlistint_t *head);
-
-/* Function to return the number of elements in a doubly linked list */
 size_t dlistint_len(const dlistint_t *h);
-
-/* Function to get the nth node of a doubly linked list */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
-
-/* Function to compute the sum of all the data (n) in a doubly linked list */
 int sum_dlistint(dlistint_t *head);
-
-/* Function to insert a new node at a given position in a doubly linked list */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
-
-/* Function to delete a node at a given index in a doubly linked list */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
 #endif /* LISTS_H */
