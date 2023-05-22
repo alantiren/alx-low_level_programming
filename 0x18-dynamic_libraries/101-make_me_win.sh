390065dc6711619e4d6bf05b9f43d8f72001babe
alantiren <tirenalan0@gmail.com>
2023-05-23 01:21:10 +0300

    update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,10 +1,1 @@
 #!/bin/bash
-
-{
-/* Code here
- * printing text here
- * */
-        write(1, "9 8 10 24 75 - 9\n", 17);
-        write(1, "Congratulations, you win the Jackpot!\n", 38);
-        exit(EXIT_SUCCESS);
-}
