8a2217f0b7bb6da5966082a8ac798f8c8ffb18ee
alantiren <tirenalan0@gmail.com>
2023-05-23 00:48:55 +0300

    updates

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-write(1, "9 8 10 24 75 - 9\n", 17);
-write(1, "Congratulations, you win the Jackpot!\n", 38);
-exit(EXIT_SUCCESS);
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+int printf(const char *format, ...)
+{
+/* Code here
+ * printing text here
+ * */
+        write(1, "9 8 10 24 75 - 9\n", 17);
+        write(1, "Congratulations, you win the Jackpot!\n", 38);
+        exit(EXIT_SUCCESS);
+}
