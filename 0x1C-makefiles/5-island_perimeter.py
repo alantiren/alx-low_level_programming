6bd1f8fec3010e692a0ace9f276ef2cb04958a29
alantiren <tirenalan0@gmail.com>
2023-07-26 15:02:31 +0300

    updates

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -10,18 +10,19 @@ def island_perimeter(grid):
     Args:
         grid (list): A list of lists of integers representing the grid.
     """
-    rows = len(grid)
-    cols = len(grid[0])
-    perimeter = 0
-    for i in range(1, rows - 1):
-        for j in range(1, cols - 1):
-            if grid[i][j] == 1:
-                if grid[i - 1][j] == 0:
-                    perimeter += 1
-                if grid[i + 1][j] == 0:
-                    perimeter += 1
-                if grid[i][j - 1] == 0:
-                    perimeter += 1
-                if grid[i][j + 1] == 0:
-                    perimeter += 1
-    return perimeter
+
+width = len(grid[0])
+height = len(grid)
+edges = 0
+size = 0
+
+for i in range(height):
+for j in range(width):
+if grid[i][j] == 1:
+size += 1
+if j > 0 and grid[i][j - 1] == 1:
+edges += 1
+if i > 0 and grid[i - 1][j] == 1:
+edges += 1
+
+return (size * 4 - edges * 2)
