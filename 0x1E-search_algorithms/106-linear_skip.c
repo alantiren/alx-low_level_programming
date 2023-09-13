3b2a621f2a586a8905f0455e87013b6f6d72f418
alantiren <122862761+alantiren@users.noreply.github.com>
2023-09-13 21:28:19 +0300

    TASKS

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -11,40 +11,28 @@
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-skiplist_t *current, *express;
-
+skiplist_t *node, *jump;
 if (list == NULL)
 return (NULL);
-
-express = list;
-while (express->express)
+for (node = jump = list; jump->next != NULL && jump->n < value;)
 {
-printf("Value checked at index [%lu] = [%d]\n", express->index, express->n);
-if (express->n >= value)
-break;
-current = express;
-express = express->express;
+node = jump;
+if (jump->express != NULL)
+{
+jump = jump->express;
+printf("Value checked at index [%ld] = [%d]\n",
+jump->index, jump->n);
 }
-
-if (!express->express && express->n < value)
+else
 {
-current = express;
-while (express->next)
-{
-express = express->next;
+while (jump->next != NULL)
+jump = jump->next;
 }
 }
-
-printf("Value found between indexes [%lu] and [%lu]\n",
-current->index, express->index);
-
-while (current)
-{
-printf("Value checked at index [%lu] = [%d]\n", current->index, current->n);
-if (current->n == value)
-return (current);
-current = current->next;
-}
-
-return (NULL);
+printf("Value found between indexes [%ld] and [%ld]\n",
+node->index, jump->index);
+for (; node->index < jump->index && node->n < value; node = node->next)
+printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+return (node->n == value ? node : NULL);
 }
