cff003b0aa6ddb3bea1d3d6d46cc22e0f4389625
alantiren <122862761+alantiren@users.noreply.github.com>
2023-09-13 21:00:44 +0300

    TASKS

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -8,14 +8,13 @@
  */
 void print_array(int *array, size_t l, size_t r)
 {
-    size_t i;
-
-    if (array)
-    {
-        printf("Searching in array: ");
-        for (i = l; i < l + (r - l + 1); i++)
-            printf("%d%s", *(array + i), i < l + (r - l) ? ", " : "\n");
-    }
+size_t i;
+if (array)
+{
+printf("Searching in array: ");
+for (i = l; i < l + (r - l + 1); i++)
+printf("%d%s", *(array + i), i < l + (r - l) ? ", " : "\n");
+}
 }
 
 /**
@@ -30,20 +29,20 @@ void print_array(int *array, size_t l, size_t r)
  */
 int binary_search_index(int *array, size_t l, size_t r, int value)
 {
-    size_t m;
+size_t m;
 
-    if (!array)
-        return (-1);
-    print_array(array, l, r);
-    m = l + ((r - l) / 2);
-    if (l == r)
-        return (*(array + m) == value ? (int)m : -1);
-    if (value < *(array + m))
-        return (binary_search_index(array, l, m - 1, value));
-    else if (value == *(array + m))
-        return ((int)m);
-    else
-        return (binary_search_index(array, m + 1, r, value));
+if (!array)
+return (-1);
+print_array(array, l, r);
+m = l + ((r - l) / 2);
+if (l == r)
+return (*(array + m) == value ? (int)m : -1);
+if (value < *(array + m))
+return (binary_search_index(array, l, m - 1, value));
+else if (value == *(array + m))
+return ((int)m);
+else
+return (binary_search_index(array, m + 1, r, value));
 }
 
 /**
@@ -56,5 +55,5 @@ int binary_search_index(int *array, size_t l, size_t r, int value)
  */
 int binary_search(int *array, size_t size, int value)
 {
-    return (binary_search_index(array, 0, size - 1, value));
+return (binary_search_index(array, 0, size - 1, value));
 }
